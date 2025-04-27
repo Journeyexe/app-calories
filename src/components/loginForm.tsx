786f37f0e2cd5c7a2eaaf7ac3a74f9efb0ca6ad8
Journeyexe <jonathanvogado@gmail.com>
2025-04-27 13:51:48 -0300

    login implement

---
+++ src/components/loginForm.tsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import api from "../api/api";
+import { useNavigate } from "react-router-dom";
+
+export function LoginForm() {
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      const response = await api.post("/auth/login", {
+        email,
+        password,
+      });
+
+      const { token, id } = response.data.data;
+
+      localStorage.setItem("token", token);
+      localStorage.setItem("userId", id);
+
+      console.log("Login feito com sucesso!");
+
+      navigate("/");
+    } catch (error) {
+      console.error("Erro no login:", error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleLogin}>
+      <div>
+        <label>Email:</label>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </div>
+
+      <div>
+        <label>Senha:</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </div>
+
+      <button type="submit">Login</button>
+    </form>
+  );
+}
---
+++ src/pages/loginPage/index.tsx
@@ -0,0 +1,9 @@
+import { LoginForm } from "../../components/loginForm";
+
+export function LoginPage() {
+  return (
+    <>
+      <LoginForm />
+    </>
+  );
+}
